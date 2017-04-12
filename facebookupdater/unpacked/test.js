caedd039fced71c61d1fa226f0b6eeef4958f338
unknown <facumo.fm@gmail.com>
2017-04-12 00:01:41 -0300

    Some indentation improvement, nothing important.

--- facebookupdater/unpacked/test.js
+++ facebookupdater/unpacked/test.js
@@ -31,9 +31,9 @@ function recursiveApproval(){
 				var cancelbutton = document.getElementsByClassName('autofocus layerCancel _4jy0 _4jy3 _4jy1 _51sy selected _42ft');
 				var cancelbutton1st = cancelbutton[0];
 				if(typeof(cancelbutton1st)!='undefined' && cancelbutton1st!=null){
-				console.log('|--> Found "Close" button, clicking...');
-				cancelbutton1st.click();
-				}
+					console.log('|--> Found "Close" button, clicking...');
+						cancelbutton1st.click();
+					}
 				console.log(currentCuate.name+" repeated, retrying in one second (1 second).");
 				setTimeout(function(){
 					recursiveApproval();
