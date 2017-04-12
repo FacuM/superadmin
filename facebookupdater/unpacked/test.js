4baa60c4b1795f8b08b7f2523ccd57388a6703f4
unknown <facumo.fm@gmail.com>
2017-04-12 00:04:25 -0300

    Some indentation improvement, nothing important.

--- facebookupdater/unpacked/test.js
+++ facebookupdater/unpacked/test.js
@@ -33,7 +33,7 @@ function recursiveApproval(){
 				if(typeof(cancelbutton1st)!='undefined' && cancelbutton1st!=null){
 					console.log('|--> Found "Close" button, clicking...');
 					cancelbutton1st.click();
-					}
+				}
 				console.log(currentCuate.name+" repeated, retrying in one second (1 second).");
 				setTimeout(function(){
 					recursiveApproval();
