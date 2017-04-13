01fe9b4a2bd66b63982c84409a3fdfc958e38c28
unknown <facumo.fm@gmail.com>
2017-04-12 22:03:31 -0300

    Fix the bug where the bot would not have started sometimes.

--- facebookupdater/unpacked/test.js
+++ facebookupdater/unpacked/test.js
@@ -1,7 +1,10 @@
 var oldCuate="";
 var minTime=15552000;	//6 months since account creation.
-
-function recursiveApproval(){
+console.log("=============================");
+console.log("Waiting 5 seconds to begin...");
+console.log("=============================");
+setTimeout(function(){
+    function recursiveApproval(){
 		var  a=document.getElementsByName("approve button")[0];
 		if(typeof(a)!='undefined' && a!=null){
 			var currentCuate=processUserData(a);
@@ -45,55 +48,56 @@ function recursiveApproval(){
 		}
 	
 }
-//////Gets the DOM's name and creation date.
-function processUserData(elm){
-	var _element=elm.parentNode.parentNode;
-	_element=_element.childNodes[1];
-	var _obj={};
-	_obj.name=getName(_element);
-	_obj.date=getDate(_element);
-	return _obj;
-	//Name:
-	function getName(p){
-		var auxDiv=document.createElement("div");
-			auxDiv.innerHTML=p.innerHTML;
-		var n=auxDiv.getElementsByTagName("a")[0]
-	return n.innerText;
+	//////Gets the DOM's name and creation date.
+	function processUserData(elm){
+		var _element=elm.parentNode.parentNode;
+		_element=_element.childNodes[1];
+		var _obj={};
+		_obj.name=getName(_element);
+		_obj.date=getDate(_element);
+		return _obj;
+		//Name:
+		function getName(p){
+			var auxDiv=document.createElement("div");
+				auxDiv.innerHTML=p.innerHTML;
+			var n=auxDiv.getElementsByTagName("a")[0]
+		return n.innerText;
+		}
+		//Creation date:
+		function getDate(e){
+			var calendar={
+			"enero":"01",
+			"febrero":"02",
+			"marzo":"03",
+			"abril":"04",
+			"mayo":"05",
+			"junio":"06",
+			"julio":"07",
+			"agosto":"08",
+			"septiembre":"09",
+			"octubre":"10",
+			"noviembre":"11",
+			"diciembre":"12"
+			};
+			var dateString=e.getElementsByClassName("timestampContent")[0].innerText;
+			var splitDate=dateString.split(" de ");
+				splitDate[1]=calendar[splitDate[1]];
+			var firstDate=new Date(splitDate[2],(splitDate[1]-1),splitDate[0],0,0,0);
+				firstDate=firstDate.getTime();
+			return {printDate:dateString, timestamp: firstDate};
+		}
 	}
-	//Creation date:
-	function getDate(e){
-		var calendar={
-		"enero":"01",
-		"febrero":"02",
-		"marzo":"03",
-		"abril":"04",
-		"mayo":"05",
-		"junio":"06",
-		"julio":"07",
-		"agosto":"08",
-		"septiembre":"09",
-		"octubre":"10",
-		"noviembre":"11",
-		"diciembre":"12"
-		};
-		var dateString=e.getElementsByClassName("timestampContent")[0].innerText;
-		var splitDate=dateString.split(" de ");
-			splitDate[1]=calendar[splitDate[1]];
-		var firstDate=new Date(splitDate[2],(splitDate[1]-1),splitDate[0],0,0,0);
-			firstDate=firstDate.getTime();
-		return {printDate:dateString, timestamp: firstDate};
+
+	// Gets the button to decline a request over the Approval button.
+	function getDeclineUser(e){
+		var    _element=e.parentNode
+			_element=_element.childNodes[1];
+		return _element;
 	}
-}
+	
+	setTimeout(function(){
+		window.location.reload(1);
+	}, 60000);
 
-// Gets the button to decline a request over the Approval button.
-function getDeclineUser(e){
-	var    _element=e.parentNode
-		   _element=_element.childNodes[1];
-	return _element;
-}
-
-setTimeout(function(){
-   window.location.reload(1);
-}, 60000);
-
- recursiveApproval();
+	recursiveApproval();
+}, 5000);
